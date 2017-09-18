73bfea38a388c9b6605a98e79f257d11df1a7a01
jyn <jyn@xkeshi.com>
2017-09-18 17:36:45 +0800

    fix: remove scrap

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function(req, res) {
-  res.send('hello world!')
-}).listen(3000, function() {
-  console.log('port at 3000')
-})
