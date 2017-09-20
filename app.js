52cd26f232b2c1a38192f670a208c7823e29e375
jyn <jyn@xkeshi.com>
2017-09-20 14:26:58 +0800

    feat: add favicon and comments

--- app.js
+++ app.js
@@ -18,17 +18,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// 设置favicon路径为当前目录下的public/favicon.ico
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// 会把请求打印出来（彩色字体），请求方法，路径，状态码，耗时
 app.use(logger('dev'));
+// 接收json请求，仅仅用来解析json格式，能接受body中任何Unicode编码的字符。支持自动的解析gzip和zlib。
 app.use(bodyParser.json());
+// 接收form请求，用来解析body中的urlencoded字符，只支持utf-8的编码的字符，同样也支持自动的解析gzip和zlib。
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+// 映射静态目录，实际目录当前文件夹下的public/stylesheets/style.css，访问目录是/stylesheets/style.css
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 路由控制
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
+// 404处理
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -36,6 +43,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
+// 上一个use中的next中的参数err在这里作为第一个参数传入
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
